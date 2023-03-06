2c89af141a60ad265ccf0429c5e09a90a297ea59
sooho-shin <sooho.shin@monoverse.io>
2023-03-06 19:01:57 +0900

    ㅇㅇ

--- src/routes/styledComponent/styles.css.ts
+++ src/routes/styledComponent/styles.css.ts
@@ -1,7 +1,6 @@
 import { style } from "styled-vanilla-extract/qwik";
 
 export const parent = style({
-  /* 스타일 속성 정의 */
   width: "300px",
   height: "200px",
   backgroundColor: "green",
@@ -15,7 +14,6 @@ export const child = style({
 });
 
 export const test2 = style({
-  /* 스타일 속성 정의 */
   width: "100px",
   height: "50px",
   backgroundColor: "green",
